3e76ce5a1b06a8aeded78aeea561e86f5031fdbb
anyawhitman1018 <annawhitman@ufl.edu>
2023-10-25 09:44:12 -0400

    first commit

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+#print the menu
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def menu():
+    print("Menu")
+    print("------------")
+    print("1. Encode")
+    print("2. Decode")
+    print("3. Quit")
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
